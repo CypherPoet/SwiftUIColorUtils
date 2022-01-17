752b49a7e990547fee4e03df7aa8c23efec18cf1
CypherPoet <CypherPoet@gmail.com>
2022-01-16 20:09:43 -0600

    Remove old `ThemeColorGroup` tests and related types.

--- Tests/SwiftUIColorUtils/Toolbox/TestConstants.swift
+++ Tests/SwiftUIColorUtils/Toolbox/TestConstants.swift
@@ -2,18 +2,4 @@ import SwiftUI
 
 
 enum TestConstants {
-    
-    enum ThemeColors {
-        
-        enum Group1 {
-            static let primary = Color("Theme1-Primary", bundle: .module)
-            static let secondary1 = Color("Theme1-Secondary1", bundle: .module)
-            static let secondary2 = Color("Theme1-Secondary2", bundle: .module)
-            static let secondary3 = Color("Theme1-Secondary3", bundle: .module)
-            static let pop1 = Color("Theme1-Pop1", bundle: .module)
-            static let pop2 = Color("Theme1-Pop2", bundle: .module)
-            static let pop3 = Color("Theme1-Pop3", bundle: .module)
-        }
-        
-    }
 }
